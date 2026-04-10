2760f0172a48febda22421b8d3a1b7071f251a98
Muhammad Talha <m.talha.shafiq95@gmail.com>
2026-04-11 00:06:49 +0500

    level 2 ci cd added reports and screenshots

--- test_google.py
+++ test_google.py
@@ -1,11 +1,20 @@
+import os
 from playwright.sync_api import sync_playwright
+import time
 
 def test_google():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto("https://www.google.com")
-        
-        assert "Google" in page.title()
-        
-        browser.close()
+
+        try:
+            page.goto("https://example.com")
+            assert "Example" in page.title()
+
+        except Exception:
+            page.screenshot(path="failure.png")  # 🔥 screenshot on fail
+            raise
+
+        finally:
+            time.sleep(5)  # 💤 wait a bit before closing
+            browser.close()
